60e0ab8cf4d24bf70fcccd9b7b08c76f358a3b9a
JongWoo Ha <jongwooha98@gmail.com>
2021-06-23 21:26:01 +0900

    use axios to get json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
+  const [allQuotes, setAllQuotes] = useState('');
+  const [currentQuote, setCurrentQuote] = useState({
+    quote: 'Random Quote',
+    author: '',
+  });
+
+  // Get QuoteData using axios
+  const url = 'https://type.fit/api/quotes';
+  const getQuoteData = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        const quoteData = response.data;
+        setAllQuotes(quoteData);
+      })
+      .catch((error) => {
+        console.log(`Error: ${error}`);
+      });
+  };
+
+  useEffect(() => {
+    getQuoteData();
+  }, []);
+
+  // New Quote Button onClick
+  const randomIndex = Math.floor(Math.random() * allQuotes.length);
+  const randomQuote = allQuotes[randomIndex];
+  const newQuote = () => {
+    setCurrentQuote({
+      quote: randomQuote.text,
+      author: randomQuote.author,
+    });
+  };
+
+  // Share on Twitter
+  const twitterUrl =
+    'https://twitter.com/intent/tweet?text=' +
+    '"' +
+    currentQuote.quote +
+    '" ' +
+    currentQuote.author;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Random Quote Generator</h1>
+      <div id="quote-box">
+        <div className="quote">
+          <div className="text" id="text">
+            <p>{currentQuote.quote}</p>
+          </div>
+          <div className="author" id="author">
+            <p> &#8212; {currentQuote.author}</p>
+          </div>
+        </div>
+
+        <div className="buttons">
+          <button id="new-quote" onClick={newQuote}>
+            New Quote
+          </button>
+          <button>
+            <a
+              id="tweet-quote"
+              className="twitter-share-button"
+              href={twitterUrl}
+              target="top"
+            >
+              <i className="fab fa-twitter"></i>
+              &nbsp;Tweet Quote
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
